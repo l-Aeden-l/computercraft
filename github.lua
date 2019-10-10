93a36241e987c939aaf4cade9e336ff6def2f315
Aeden <hauguel.frederic@gmail.com>
2019-10-10 15:13:57 +0200

    Restauration code d'origine en gardant la nouvelle adresse raw Github

--- github.lua
+++ github.lua
@@ -1,33 +1,20 @@
 local args = {...}
-local h = nil
 
-if ( not(#args >= 4) ) then
-  error("Usage: "..shell.getRunningProgram().." <user> <repo> <branch> [<path>] <save file>")
+if #args ~= 5 then
+  error("Usage: "..shell.getRunningProgram().." <user> <repo> <branch> <path> <save file>")
 end
 
-if #args == 4 then
-  h = http.get("https://raw.githubusercontent.com/"..args[1].."/"..args[2].."/"..args[3].."/"..args[4]).readAll()
-else
-  h = http.get("https://raw.githubusercontent.com/"..args[1].."/"..args[2].."/"..args[3].."/"..args[4].."/"..args[5]).readAll()
-end
+local h = http.get("https://raw.githubusercontent.com/"..args[1].."/"..args[2].."/"..args[3].."/"..args[4]).readAll()
 
 if h then
-
-  if #args == 4 then
-    file = args[4]
-  else
-    file = args[5]
-  end
-
-  if not fs.exists(file) then
-        f = fs.open(file, "w")
+  if not fs.exists(args[5]) then
+        f = fs.open(args[5], "w")
         f.write(h)
         f.close()
-        print("File '"..file.."' downloaded.")
+        print("File '"..args[5].."' downloaded.")
   else
-        error("File '"..file.."' already exists.")
+        error("File '"..args[5].."' already exists.")
   end
-
 else
   error("Could not download file!")
 end
